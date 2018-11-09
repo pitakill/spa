7bc65f07a6bd9b950e188dcf3b68a6ffe3d8c5b8
Lepoldo Caballero <git@pitakill.net>
2018-11-09 16:06:06 -0600

    Cleanup project & Add Flow

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @flow
+import * as React from 'react'
+import type { AppProps } from './App.types'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = (props: AppProps): React.Element<"div"> => <div>Hola</div>
 
 export default App;
